13c76fc04e9d230f440d5e13783bf4188a4c49f6
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-09-19 12:47:53 +0200

    Ejercicios

---
+++ Programación/1º Evaluación/Ejercicios/Ejercicio_1_1.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Eduardo Martín-Sonseca (maraloeDev)
+ */
+package Ejercicios;
+
+public class Ejercicio_1_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo!!!");
+
+    }
+}
