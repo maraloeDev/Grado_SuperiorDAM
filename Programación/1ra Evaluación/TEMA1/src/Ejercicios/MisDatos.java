75bf1bd3ba064763084d5f3a3778def9c622112f
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-09-22 12:17:44 +0200

    EJERCICIOS

---
+++ Programación/1ra Evaluación/TEMA1/src/Ejercicios/MisDatos.java
@@ -0,0 +1,12 @@
+package Ejercicios;
+
+public class MisDatos {
+
+    public static void main(String[] args) {
+
+        System.out.println("Nombre: Eduardo");
+        System.out.println("Apellidos: Martin-Sonseca");
+        System.out.println("Dirección: C/La Malaza,40");
+        System.out.println("Telefono: 601094161");
+    }
+}
---
+++ Programación/1ra Evaluación/TEMA1/src/Ejercicios/Saludo.java
@@ -0,0 +1,8 @@
+package Ejercicios;
+
+public class Saludo {
+
+    public static void main(String[] args) {
+        System.out.println("Me llamo Eduardo");
+    }
+}
