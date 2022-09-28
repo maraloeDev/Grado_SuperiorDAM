e20dcadee5da69484fb88b6ec88f64cc99efe9bd
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-09-28 12:25:27 +0200

    P

--- Programación/1ra Evaluación/TEMA1/src/Ejercicios/Inicial.java
+++ Programación/1ra Evaluación/TEMA1/src/Ejercicios/Inicial.java
@@ -2,16 +2,8 @@
  * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
  ******************************************************************************/
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Ejercicios;
 
-/**
- *
- * @author maraloed
- */
 public class Inicial {
 
     public static void main(String[] args) {
--- Programación/1ra Evaluación/TEMA1/src/Ejercicios/MuestraFecha.java
+++ Programación/1ra Evaluación/TEMA1/src/Ejercicios/MuestraFecha.java
@@ -2,18 +2,10 @@
  * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
  ******************************************************************************/
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Ejercicios;
 
 import java.util.Date;
 
-/**
- *
- * @author maraloed
- */
 public class MuestraFecha {
 
     static Date hoy = new Date();
