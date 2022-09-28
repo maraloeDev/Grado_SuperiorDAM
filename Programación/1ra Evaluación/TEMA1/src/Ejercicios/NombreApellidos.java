62cee6997b53809da55ce26a91c66f663a5df68c
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-09-28 12:53:07 +0200

    VAR

--- Programación/1ra Evaluación/TEMA1/src/Ejercicios/NombreApellidos.java
+++ Programación/1ra Evaluación/TEMA1/src/Ejercicios/NombreApellidos.java
@@ -7,7 +7,15 @@ package Ejercicios;
 public class NombreApellidos {
 
     public static void main(String[] args) {
-        System.out.println("Mi nombre es " + " Pepe " + " Soplillo" + ".");
+
+        String a ="Mi ";
+        String b = " Nombre ";
+        String c = "Pepe ";
+        String d = "Soplillo";
+        String e = ".";
+        System.out.println(a + b + c + d + e);
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("Mi "+ " nombre " + " es " + " Pepe " + " Soplillo " + ".");
     }
 
 }
