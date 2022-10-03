c4caf127deaff861797fa8ecac9e4ce511c58e1f
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-03 21:27:57 +0200

    EJER3

--- Programación/1ra Evaluación/Tema2/src/Ejercicios/Ejercicio_3_Calculo.java
+++ Programación/1ra Evaluación/Tema2/src/Ejercicios/Ejercicio_3_Calculo.java
@@ -18,15 +18,55 @@ public class Ejercicio_3_Calculo {
         System.out.println("Introduce un  numero");
         int numero2 = teclado.nextInt();
 
-        //HACER CON OPERADOR TERNARIO
+
+            //HACER CON OPERADOR TERNARIO
+
+        System.out.println("\tSelecciona un operador \n +\n -\n *\n / \n %");
+
+        String operador = teclado.next();
+
+        int eleccion =
+
+                (operador == "+") ? (numero1 + numero2) :
+                (operador == "-") ? (numero1 - numero2) :
+                (operador == "*") ? (numero1 * numero2) :
+                (operador == "/") ? (numero1 / numero2) :
+                (operador == "%") ? (numero1 % numero2) : 0;
+        System.out.println("-----------------------------");
+
+        System.out.println(eleccion);
 
 
-        //HACER CON SWITCH
 
-        System.out.println("\tSUMA " + (numero1 + numero2));
-        System.out.println("\tRESTA " + (numero1 - numero2));
-        System.out.println("\tMULTIPLICACION " + (numero1 * numero2));
-        System.out.println("\tDIVISION " + (numero1 / numero2));
-        System.out.println("\tRESTO " + (numero1 % numero2));
+             //HACER CON SWITCH
+
+        //System.out.println("\tSelecciona un operador \n\t 1. +\n\t 2. -\n\t 3. *\n\t 4. /\n\t 5. %");
+//
+        //int eleccion =teclado.nextInt();
+//
+        //switch (eleccion){
+//
+        //    case 1 :
+        //        System.out.println((numero1 + numero2));
+//
+        //    case 2 :
+        //        System.out.println((numero1 - numero2));
+//
+        //    case 3 :
+        //        System.out.println((numero1 * numero2));
+//
+        //    case 4 :
+        //        System.out.println((numero1 / numero2));
+//
+        //    case 5 :
+        //        System.out.println((numero1 % numero2));
+        //}
+
+        //NORMAL
+        //System.out.println("\tSUMA " + (numero1 + numero2));
+        //System.out.println("\tRESTA " + (numero1 - numero2));
+        //System.out.println("\tMULTIPLICACION " + (numero1 * numero2));
+        //System.out.println("\tDIVISION " + (numero1 / numero2));
+        //System.out.println("\tRESTO " + (numero1 % numero2));
     }
 }
