7f6b060b96d5f63323b76ed09463a2414accc41a
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-10 12:40:30 +0200

    EJ

--- Programación/1ra Evaluación/Tema2/src/Ejercicios/Ejercicio__13_A_Mayúsculas.java
+++ Programación/1ra Evaluación/Tema2/src/Ejercicios/Ejercicio__13_A_Mayúsculas.java
@@ -1,7 +1,6 @@
-/*******************************************************************************
+/** *****************************************************************************
  * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
- ******************************************************************************/
-
+ ***************************************************************************** */
 package Ejercicios;
 
 import java.util.Scanner;
@@ -13,12 +12,12 @@ public class Ejercicio__13_A_Mayúsculas {
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Introduze un caracter en  minusculas del alfabeto de la a a la z : ");
-
         char caracter = teclado.next().charAt(0);
+//Comprobación de minusculas
+        String resultado1 = (caracter >=97 && caracter<=122) ? "Su mayuscula es :" + (char)(caracter-32) 
+                : "Error";
+        System.out.println(resultado1);
         
-        String eleccion = (caracter >= 97) && (caracter <= 122) ? "El caracter " + caracter + " esta en minusculas" :
-                          (caracter >= 65) && (caracter <= 90)  ? "El caracter " + caracter + " esta en mayusculas" : "Esta fuera de rango";
 
-        System.out.println(eleccion);
     }
 }
