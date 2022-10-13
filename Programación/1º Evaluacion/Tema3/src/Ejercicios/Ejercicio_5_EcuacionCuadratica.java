5e224105a05cd2575e32158db003ff55532b06d8
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-13 13:31:14 +0200

    d

---
+++ Programación/1º Evaluacion/Tema3/src/Ejercicios/Ejercicio_5_EcuacionCuadratica.java
@@ -0,0 +1,25 @@
+
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio_5_EcuacionCuadratica  {
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce el valor de a ");
+        int a = teclado.nextInt();
+        
+        System.out.println("Introduce el valor de x ");
+        int x = teclado.nextInt();
+        
+        System.out.println("Introduce el valor de b ");
+        int b = teclado.nextInt();
+        
+        System.out.println("Introduce el valor de c ");
+        int c = teclado.nextInt();
+        
+       
+    }
+}
