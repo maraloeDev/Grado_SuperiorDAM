4313aef0e8960aff62f77d0b2f47a62d849b3195
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-13 12:53:09 +0200

    rdf

---
+++ Programación (NetBeans)/Tema3/src/Ejercicios/Ejercicio_4_OperacionesAritmeticas.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ ******************************************************************************/
+
+package Tema3.src.Ejercicios;
+
+import com.sun.source.tree.WhileLoopTree;
+
+public class Ejercicio_4_OperacionesAritmeticas {
+
+    public static void main(String[] args) {
+
+        int aleatorio = (int) (9 * Math.random() + 1);
+        int aleatorio2 = (int) (9 * Math.random() + 1);
+        System.out.println(" El numero " + aleatorio + " + " + aleatorio2 + " es  " + (aleatorio + aleatorio2));
+        System.out.println(" El numero " + aleatorio + " - " + aleatorio2 + " es  " + (aleatorio - aleatorio2));
+        System.out.println(" El numero " + aleatorio + " * " + aleatorio2 + " es  " + (aleatorio * aleatorio2));
+        System.out.println(" El numero " + aleatorio + " / " + aleatorio2 + " es  " + (aleatorio / aleatorio2));
+        System.out.println(" El numero " + aleatorio + " % " + aleatorio2 + " es  " + (aleatorio % aleatorio2));
+
+
+    }
+
+
+}
---
+++ Programación (NetBeans)/Tema3/src/EjerciciosPropuestos/Ejercicio_1_SentenciaIfElse_SignoNumero.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ ******************************************************************************/
+
+package Tema3.src.EjerciciosPropuestos;
+
+import java.util.Scanner;
+
+public class Ejercicio_1_SentenciaIfElse_SignoNumero {
+
+
+    // Hacer un programa llamado SignoNumero que reciba por teclado un número real
+    // e imprima por pantalla si el número es positivo, negativo o cero.
+    
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce un numero: ");
+        double n1 = teclado.nextInt();
+
+        if (n1 > 0) {
+            System.out.println(n1 + " es positivo");
+        } else if (n1 < 0) {
+            System.out.println(n1 + " es negativo");
+        } else {
+            System.out.println(n1 + " es cero");
+        }
+    }
+}
