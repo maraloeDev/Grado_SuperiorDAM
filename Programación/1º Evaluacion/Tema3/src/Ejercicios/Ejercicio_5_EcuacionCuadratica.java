c1f61d89900507fdf4f4bd7ce91b43bfb3d2a672
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-14 12:27:44 +0200

    rdf

--- Programación/1º Evaluacion/Tema3/src/Ejercicios/Ejercicio_5_EcuacionCuadratica.java
+++ Programación/1º Evaluacion/Tema3/src/Ejercicios/Ejercicio_5_EcuacionCuadratica.java
@@ -18,12 +18,21 @@ public class Ejercicio_5_EcuacionCuadratica {
 
         System.out.println("Introduce el valor de c ");
         int c = teclado.nextInt();
-
+        
+        double x1 = 0;
+        double x2 = 0;
+        
+        if( a ==0 || (b == 0) || (c == 0)){
+            System.out.println("Ningun coeficiente el cero");
+        } else{
+            
         double potencia = Math.pow(b, 2) - (4 * a * c);
 
-        double x1 = (-b - Math.sqrt(potencia) / 2 * a);
+         x1 = (-b - Math.sqrt(potencia) / 2 * a);
 
-        double x2 = (-b + Math.sqrt(potencia) / 2 * a);
+         x2 = (-b + Math.sqrt(potencia) / 2 * a);
+        }
+
 
         System.out.println("\nX1 = " + x1);
 
