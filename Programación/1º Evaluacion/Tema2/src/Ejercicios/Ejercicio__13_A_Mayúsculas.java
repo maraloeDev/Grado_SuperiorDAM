0c315b3377b7c0cbf3b7aeed261f5f99a4a4298a
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-15 13:57:19 +0200

    d

--- Programación/1º Evaluacion/Tema2/src/Ejercicios/Ejercicio__13_A_Mayúsculas.java
+++ Programación/1º Evaluacion/Tema2/src/Ejercicios/Ejercicio__13_A_Mayúsculas.java
@@ -9,15 +9,16 @@ public class Ejercicio__13_A_Mayúsculas {
 
     public static void main(String[] args) {
 
+        // Realizar un programa Java que solicite al usuario un carácter en minúsculas
+        //del alfabeto [a-z]. Y a continuación mostrar el mismo carácter pero en mayúsculas.
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Introduze un caracter en  minusculas del alfabeto de la a a la z : ");
         char caracter = teclado.next().charAt(0);
 //Comprobación de minusculas
-        String resultado1 = (caracter >=97 && caracter<=122) ? "Su mayuscula es :" + (char)(caracter-32) 
+        String resultado1 = (caracter >= 97 && caracter <= 122) ? "Su mayuscula es :" + (char) (caracter - 32)
                 : "Error";
         System.out.println(resultado1);
-        
 
     }
 }
--- Programación/1º Evaluacion/Tema3/src/EjerciciosPropuestos/Ejercicio_2_IVA.java
+++ Programación/1º Evaluacion/Tema3/src/EjerciciosPropuestos/Ejercicio_2_IVA.java
@@ -1,48 +1,60 @@
+/** *****************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ ***************************************************************************** */
 package EjerciciosPropuestos;
 
 import java.util.Scanner;
-import java.util.SortedMap;
 
 public class Ejercicio_2_IVA {
 
-    static  Scanner teclado = new Scanner(System.in);
+    static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        //Realiza un programa que solicite al usuario una cantidad (real) 
+        //y el tipo de IVA (G: 21%, R: 10%, S: 4%), y muestre el desglose de la siguiente forma:
+        //
+        //BI: (base imponible -> total menos el IVA)
+        //
+        //IVA: (% de IVA aplicado sobre la base imponible)
+        //
+        //Por ejemplo, si la cantidad es 1210 y el IVA es G, el resultado del programa será:
+        //
+        //BI: 1000
+        //
+        //IVA: 210
         System.out.println("Introduce una cantidad en euros ");
-    int cantidad = teclado.nextInt();
-    double G = (int)(cantidad / 1.21);
-    double R = (int)(cantidad * 1.10);
-    double S = (int)(cantidad * 1.4);
+        double bi = teclado.nextDouble();
+        
+        double G = (bi * 0.21);
+        double R = (bi * 0.10);
+        double S = (bi * 0.04);
 
         System.out.println("--------------");
 
-        System.out.println("Introduce el tipo de IVA " + "\n" +
-                " G: 21% " + "\n" +
-                " R: 10% " + "\n" +
-                " S: 4% ");
+        System.out.println("Introduce el tipo de IVA " + "\n"
+                + " G: 21% " + "\n"
+                + " R: 10% " + "\n"
+                + " S: 4% ");
         String eleccion = teclado.next();
 
-        switch (eleccion){
+        switch (eleccion) {
 
-            case "G":
-                System.out.println("BI : " + cantidad + "\n"+
-                                    "IVA : " + G);
+            case "G", "g":
+                System.out.println("BI : " + bi + "\n"
+                        + "IVA : " + G);
                 break;
-            case "R":
-                System.out.println("BI : " + cantidad + "\n"+
-                        "IVA : " + R);
+            case "R", "r":
+                System.out.println("BI : " + bi + "\n"
+                        + "IVA : " + R);
                 break;
-            case "S":
-                System.out.println("BI : " + cantidad + "\n"+
-                        "IVA : " + S);
+            case "S", "s":
+                System.out.println("BI : " + bi + "\n"
+                        + "IVA : " + S);
                 break;
             default:
                 System.out.println("Numero no contenplado ");
         }
-
-
-
 
     }
 }
