43db04f940cee2d6eac53afc0625990d07497219
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-17 14:00:37 +0200

    a

--- Programación/1º Evaluacion/Tema3/src/Ejemplos/Bucles3.java
+++ Programación/1º Evaluacion/Tema3/src/Ejemplos/Bucles3.java
@@ -1,14 +1,17 @@
-/** *****************************************************************************
+/**
+ * ****************************************************************************
  * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
- ***************************************************************************** */
+ * ****************************************************************************
+ */
 package Ejemplos;
 
 import java.util.Scanner;
 
 public class Bucles3 {
 
+    //Numeros naturares entre 2 valores introducidos por teclado
+
     public static void main(String[] args) {
-        //Numeros naturares entre 2 valores introducidos por teclado 
         Scanner teclado = new Scanner(System.in);
 
         System.out.print("Escribe un numero");
@@ -17,20 +20,20 @@ public class Bucles3 {
         System.out.print("Escribe un numero");
         int num2 = teclado.nextInt();
 
-        if (num2 <= 0 || num2 <= 0) {
+        if (num < 0 || num2 < 0) {
             System.out.println("Los valores deben de ser positivos");
-        } else {
+        } else if (num >= num2) {
 
             while (num >= num2) {
                 System.out.println(num);
                 num--;
             }
 
+        } else if (num <= num2) {
             while (num <= num2) {
                 System.out.println(num);
                 num++;
             }
-
         }
     }
 }
---
+++ Programación/1º Evaluacion/Tema3/src/Ejemplos/Bucles4.java
@@ -0,0 +1,27 @@
+package Ejemplos;
+
+import java.util.Scanner;
+
+public class Bucles4 {
+
+    //Programa que cuente y sume los pares que hay entre 1 y un numero
+
+    //Hay 5 pares suman 30
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Introduce un numero : ");
+        int num = teclado.nextInt();
+        int cont = 0;
+
+        if ((num % 2) == 0) {
+
+
+        } else{
+            System.out.println("Has introducido un numero impar");
+        }
+
+
+    }
+}
