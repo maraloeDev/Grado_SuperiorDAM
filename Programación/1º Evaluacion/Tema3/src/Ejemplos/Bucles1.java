a81259482b87219b775f4159ab04a1c47c55c7f0
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-17 12:48:24 +0200

    oi

--- Programación/1º Evaluacion/Tema3/src/Ejemplos/Bucles1.java
+++ Programación/1º Evaluacion/Tema3/src/Ejemplos/Bucles1.java
@@ -1,14 +1,25 @@
-/** *****************************************************************************
+/**
+ * ****************************************************************************
  * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
- ***************************************************************************** */
-
+ * ****************************************************************************
+ */
 package Ejemplos;
 
+import java.util.Scanner;
 
 public class Bucles1 {
-    
+
     public static void main(String[] args) {
+
         //Es una secuencia de instrucciones que se ejecutan un numero determinado de veces
+        //muestra 30 veces hola.
+        int cont = 1;
+
+        while (cont <= 30) {
+
+            System.out.println(cont + " " + " hola");
+            cont++;
+        }
     }
 
 }
