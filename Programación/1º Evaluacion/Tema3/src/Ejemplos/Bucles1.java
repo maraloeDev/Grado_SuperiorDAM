337e3120fd02309cf89174a1efb8bfb6246f21bd
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-17 12:30:42 +0200

    sw

---
+++ Programación/1º Evaluacion/Tema3/src/Ejemplos/Bucles1.java
@@ -0,0 +1,14 @@
+/** *****************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ ***************************************************************************** */
+
+package Ejemplos;
+
+
+public class Bucles1 {
+    
+    public static void main(String[] args) {
+        //Es una secuencia de instrucciones que se ejecutan un numero determinado de veces
+    }
+
+}
