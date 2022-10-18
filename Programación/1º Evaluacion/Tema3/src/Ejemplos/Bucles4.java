eb704ffe69354ee8c75fd8a403a02f897ab5ddba
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-18 11:43:38 +0200

    SX

--- Programación/1º Evaluacion/Tema3/src/Ejemplos/Bucles4.java
+++ Programación/1º Evaluacion/Tema3/src/Ejemplos/Bucles4.java
@@ -1,6 +1,7 @@
+
+
 package Ejemplos;
 
-import java.awt.*;
 import java.util.Scanner;
 
 public class Bucles4 {
@@ -15,37 +16,31 @@ public class Bucles4 {
      * //
      * //  Suman 30/
      */
-
-    //Hay 5 pares suman 30
     static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        System.out.println("Introduce un numero : ");
+        System.out.println("Introduce valor : ");
         int num = teclado.nextInt();
-        int contpares = 0;
-        int contador = 2;
-        int suma = 0;
 
-        if (num <0 ){
+        if (num < 0 ){
             System.out.println("NO SE PUEDE");
         } else {
+            int contadorPares=0;
+        int contador = 0;
+        int suma=0;
 
-        while (contador <= num) {
+        while (contador < num) {
 
             System.out.println(contador);
             contador=contador+1;
-            contpares++;
             contador++;
+            suma=suma+contador;
+            contadorPares++;
 
-            suma= suma + contador;
         }
-        System.out.println("Hay " + contpares + " pares");
-        System.out.println("Suman " + (suma));
+        System.out.println("Hay " + contadorPares + " pares");
+        System.out.println("Suman " + suma);
         }
-
-
-
-
     }
 }
