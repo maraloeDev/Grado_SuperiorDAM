592703aff9c525bf40790d89ec0cf2694da60344
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-18 10:31:08 +0200

    mnj

--- Programación/1º Evaluacion/Tema3/src/Ejemplos/Bucles4.java
+++ Programación/1º Evaluacion/Tema3/src/Ejemplos/Bucles4.java
@@ -1,17 +1,20 @@
 package Ejemplos;
 
+import java.awt.*;
 import java.util.Scanner;
 
 public class Bucles4 {
 
-    /**Programa que muestre. cuente. sume los pares que hay entre 1 y un numero positivo
-    // introducido por teclado
-    //  Introduce valor : 10
-    //  2 4 6 8 10
-    //
-    //  Hay : 5 pares
-    //
-    //  Suman 30/*/
+    /**
+     * Programa que muestre. cuente. sume los pares que hay entre 1 y un numero positivo
+     * // introducido por teclado
+     * //  Introduce valor : 10
+     * //  2 4 6 8 10
+     * //
+     * //  Hay : 5 pares
+     * //
+     * //  Suman 30/
+     */
 
     //Hay 5 pares suman 30
     static Scanner teclado = new Scanner(System.in);
@@ -20,6 +23,26 @@ public class Bucles4 {
 
         System.out.println("Introduce un numero : ");
         int num = teclado.nextInt();
+        int contpares = 0;
+        int contador = 2;
+        int suma = 0;
+
+        if (num <0 ){
+            System.out.println("NO SE PUEDE");
+        } else {
+
+        while (contador <= num) {
+
+            System.out.println(contador);
+            contador=contador+1;
+            contpares++;
+            contador++;
+
+            suma= suma + contador;
+        }
+        System.out.println("Hay " + contpares + " pares");
+        System.out.println("Suman " + (suma));
+        }
 
 
 
