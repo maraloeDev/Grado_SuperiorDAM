b1f81eb4e738f15f6132f9c2d362605a11b0aa50
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-20 13:55:41 +0200

    sfd

--- Programación/1º Evaluacion/Tema3/src/Ejemplos/Ejemplo_Banderas2.java
+++ Programación/1º Evaluacion/Tema3/src/Ejemplos/Ejemplo_Banderas2.java
@@ -9,17 +9,23 @@ import java.util.Scanner;
 public class Ejemplo_Banderas2 {
 
     /**
-     * Una bandera es una variable booleana que ouede tener dos posibles valores
+     * Una bandera es una variable booleana que puede tener dos posibles valores
      * Usos
      * - Bucles para controlar la permanencia.
      * - if-else para ejecutar instrucciones dependiendo de su valor.
      * <p>
+     *
+     *
      * Ejercicio
      * Programa que pida al usuario repetidamente números hasta que introduzca 0
      * Para cada número introducido, tendra que decir si es par o impar
      */
 
+    public static void main(String[] args) {
 
+
+
+    }
 
 
 }
