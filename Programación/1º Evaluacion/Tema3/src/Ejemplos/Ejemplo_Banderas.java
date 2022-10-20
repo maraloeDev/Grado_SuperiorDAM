aaea04bc8b60dc545dc0912a49fa0339aa73115b
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-20 12:53:11 +0200

    df

--- Programación/1º Evaluacion/Tema3/src/Ejemplos/Ejemplo_Banderas.java
+++ Programación/1º Evaluacion/Tema3/src/Ejemplos/Ejemplo_Banderas.java
@@ -32,22 +32,23 @@ public class Ejemplo_Banderas {
         System.out.print("Frase2 : ");
         String Frase2 = teclado.nextLine();
         double aleatorio = (int) (10 * Math.random() + 10);
-        boolean bandera1 = true;
-        int contador = 0;
-        while (contador < aleatorio) {
+        boolean bandera1 = true; //Controla la alternancia entre las frases
+        int contador = 0; //Contador de veces que se muestra las frases
+
+        while (contador <= aleatorio) {
 
             if (bandera1 == true) {
-                System.out.println("Frase1: " + Frase1);
+                System.out.println(contador + " " + " Frase1: " + Frase1);
                 bandera1 = false;
             } else {
-                System.out.println("Frase2: " + Frase2);
+                System.out.println( contador + " " +" Frase2: " + Frase2);
                 bandera1 = true;
-
-            }
             contador++;
-        }
-        System.out.println("El numero de veces introducidas es de " + contador);
+            }
 
+        }
+
+        System.out.println("Veces " + aleatorio);
     }
 
 }
---
+++ Programación/1º Evaluacion/Tema3/src/Ejemplos/Ejemplo_Banderas2.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ ******************************************************************************/
+
+package Ejemplos;
+
+import java.util.Scanner;
+
+public class Ejemplo_Banderas2 {
+
+    /**
+     * Una bandera es una variable booleana que ouede tener dos posibles valores
+     * Usos
+     * - Bucles para controlar la permanencia.
+     * - if-else para ejecutar instrucciones dependiendo de su valor.
+     * <p>
+     * Ejercicio
+     * Programa que pida al usuario repetidamente números hasta que introduzca 0
+     * Para cada número introducido, tendra que decir si es par o impar
+     */
+
+
+
+
+}
