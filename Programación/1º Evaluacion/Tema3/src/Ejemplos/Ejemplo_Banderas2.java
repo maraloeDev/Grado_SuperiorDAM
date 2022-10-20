9a622b257a4de895bd2a236e9c102a47991603a2
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-20 20:19:33 +0200

    e

--- Programación/1º Evaluacion/Tema3/src/Ejemplos/Ejemplo_Banderas2.java
+++ Programación/1º Evaluacion/Tema3/src/Ejemplos/Ejemplo_Banderas2.java
@@ -5,6 +5,7 @@
 package Ejemplos;
 
 import java.util.Scanner;
+
 @SuppressWarnings("ALL")
 public class Ejemplo_Banderas2 {
 
@@ -14,8 +15,8 @@ public class Ejemplo_Banderas2 {
      * - Bucles para controlar la permanencia.
      * - if-else para ejecutar instrucciones dependiendo de su valor.
      * <p>
-     *
-     *
+     * <p>
+     * <p>
      * Ejercicio
      * Programa que pida al usuario repetidamente números hasta que introduzca cero
      * Para cada número introducido, tendra que decir si es par o impar
@@ -27,17 +28,27 @@ public class Ejemplo_Banderas2 {
 
         System.out.println("Introduce un numero");
         int numero = teclado.nextInt();
-        int contador=0;
+        int contador = 0;
 
-        while (numero != 0){
+        boolean bandera1 = true;
+
+        while (numero != 0) {
             System.out.println("Introduce un numero");
-            numero= teclado.nextInt();
-contador++;
-        }
-            if ((numero%2)==0){
-                System.out.println("Es par");
+            numero = teclado.nextInt();
+            if (numero < 0){
+                System.out.println("ES UN NUMERO NEGATIVO!!");
             }
-        System.out.println("Has acabado");
+        else if ((numero % 2) == 0) {
+                System.out.println("Es par");
+                bandera1 = true;
+            } else {
+                System.out.println("Es impar");
+                bandera1 = false;
+            }
+            contador++;
+
+        }
+        System.out.println("Has acabado. Has introducido " + contador + " pares");
     }
 
 
