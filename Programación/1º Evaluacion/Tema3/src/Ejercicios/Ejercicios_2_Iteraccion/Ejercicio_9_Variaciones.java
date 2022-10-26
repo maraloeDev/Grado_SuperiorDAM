d93234167b1e4a09b224b4c5f8cbe890bda39f2b
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-10-26 13:21:15 +0200

    a

---
+++ Programación/1º Evaluacion/Tema3/src/Ejercicios/Ejercicios_2_Iteraccion/Ejercicio_9_Variaciones.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ ******************************************************************************/
+
+package Ejercicios.Ejercicios_2_Iteraccion;
+
+public class Ejercicio_9_Variaciones {
+
+    public static void main(String[] args) {
+
+        for (int i = 1111; i <= 1114; i++) {
+            System.out.println(i);
+        }
+        System.out.println("-------------------------");
+            for (int j = 2221; j <= 2224; j++) {
+                System.out.println(j);
+            }
+        System.out.println("-------------------------");
+                for (int k = 3331; k <= 3334; k++) {
+                    System.out.println(k);
+                }
+        System.out.println("-------------------------");
+                    for (int l = 4441; l <= 4444; l++) {
+                        System.out.println(l);
+
+                    }
+
+                }
+
+            }
