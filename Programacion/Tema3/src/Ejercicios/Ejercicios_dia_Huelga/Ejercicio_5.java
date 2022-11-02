e3b753387ed71d05cde3db52d09bb71aba535b67
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-11-02 13:52:54 +0100

    a

--- Programacion/Tema3/src/Ejercicios/Ejercicios_dia_Huelga/Ejercicio_5.java
+++ Programacion/Tema3/src/Ejercicios/Ejercicios_dia_Huelga/Ejercicio_5.java
@@ -13,21 +13,7 @@ public class Ejercicio_5 {
 
         int contador=0;
         int veces = 0;
-        while (contador < 100){
-            int dado1 = (int) (Math.random() *6) +1 ;
-            int dado2 = (int) (Math.random() *6) +1 ;
-
-            System.out.println("Tirada " + contador + " " + 1);
-            System.out.println("------------------------------------");
-            System.out.println("Dado 1 " + dado1);
-            System.out.println("Dado 2 " + dado2);
-
-            if (dado1 + dado2==10){
-                veces++;
-            }else {
-                contador++;
-            }
-        }
+        
         System.out.println("Ha salido el 10 " + veces + " veces");
 
 
