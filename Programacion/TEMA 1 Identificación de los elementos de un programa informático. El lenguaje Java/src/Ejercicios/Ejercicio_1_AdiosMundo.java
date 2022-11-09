9163834428e3bb78e2ba52f8a0eda82643a9b2ba
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-11-09 12:51:25 +0100

    js

---
+++ Programacion/TEMA 1 Identificación de los elementos de un programa informático. El lenguaje Java/src/Ejercicios/Ejercicio_1_AdiosMundo.java
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ ******************************************************************************/
+
+package Ejercicios;
+
+public class Ejercicio_1_AdiosMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Adios Mundo Cruel!!!");
+    }
+
+}
---
+++ Programacion/TEMA 1 Identificación de los elementos de un programa informático. El lenguaje Java/src/Ejercicios/Ejercicio_8_NombreApellidos.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ ******************************************************************************/
+
+package Ejercicios;
+
+public class Ejercicio_8_NombreApellidos {
+
+    public static void main(String[] args) {
+
+        String a ="Mi ";
+        String b = " Nombre ";
+        String c  =" es ";
+        String d = "Pepe ";
+        String e = "Soplillo";
+        String f = ".";
+        System.out.println(a + b + c + d + e + f);
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("Mi "+ " nombre " + " es " + " Pepe " + " Soplillo " + ".");
+    }
+
+}
