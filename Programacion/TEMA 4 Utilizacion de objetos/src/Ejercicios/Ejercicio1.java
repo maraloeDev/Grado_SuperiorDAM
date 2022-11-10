5fdb114248dc6cc84804d0e402c74c5114de4889
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-11-10 12:08:55 +0100

    z

--- Programacion/TEMA 4 Utilizacion de objetos/src/Ejercicios/Ejercicio1.java
+++ Programacion/TEMA 4 Utilizacion de objetos/src/Ejercicios/Ejercicio1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Ejemplos;
+package Ejercicios;
 
 import java.util.Random;
 
@@ -21,12 +21,12 @@ import java.util.Random;
  */
 public class Ejercicio1 {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Random random = new Random();
 
-        char caracterMayus = (char) (random.nextInt(65, 90)); // Caracteres entre a y z
-        char caracterMinus = (char) (random.nextInt(97, 122)); // Caracteres entre A y Z
-        int numero = random.nextInt(1, 25); //Numeros aleatorios entre 1 y 25
+        int numero = random.nextInt(25) + 1; //Generador de numeros aleatorios entre 1 y 25
+        char caracterMayus = (char) (random.nextInt(65, 90)); // Generador de caracteres entre a y z
+        char caracterMinus = (char) (random.nextInt(97, 122)); // Generador de caracteres entre A y Z
 
         String caraMayus = String.valueOf(caracterMayus);
         String caraMinus = String.valueOf(caracterMinus);
@@ -36,9 +36,24 @@ public class Ejercicio1 {
         int totalMin = caraMinus.length();
         int totnum = num.length();
 
-        System.out.println("Palabra generada (tamano " + (totalMay + totalMin + totnum) + ")"
+        System.out.println("Palabra generada (tamano " + (totalMay + totnum + totalMay) + ")"
                 + caracterMayus + "" + caracterMinus + "" + numero);
 
+    }*/
+    
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int numero = random.nextInt(25) + 1; //Generador de numeros aleatorios entre 1 y 25
+        int aleatorio=0;
+        String resultado= "";
+        int i=0;
+        for ( i = 0; i < numero; i++) {
+            aleatorio= random.nextInt(94) + 33;
+            
+            resultado = resultado + (char) aleatorio;
+        }
+        System.out.println("Palabra generada ( tamano " + i + ") " + resultado);
     }
 
 }
