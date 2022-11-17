35fcc4f833ae2d7f156739b12c318dfa5594adb5
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-11-17 12:59:57 +0100

    w

--- Programacion/TEMA 4 Utilizacion de objetos/src/Ejemplos/FormateoFechas1.java
+++ Programacion/TEMA 4 Utilizacion de objetos/src/Ejemplos/FormateoFechas1.java
@@ -10,26 +10,34 @@ import java.util.Locale;
 public class FormateoFechas1 {
     
     public static void main(String[] args) {
-        //Uso de date format -> 
-
-        DateFormat df = DateFormat.getDateInstance();
+        
+        //DateFormat -> nos limitamos a las opciones FULL, LONG, MEDIUM, SHORT
+        
+        //Creamos el formateador de fecha
+        DateFormat df = DateFormat.getInstance();
+        
+        //Creamos objeto Date
         Date hoy = new Date();
-        System.out.println("Formato por defecto " + df.format(hoy));
         
+        //Formateamos el objeto "Date" con el formateador "df" 
+        System.out.println("Formato por defecto (short): " + df.format(hoy));
+        
+        //Cambiamos el formateador al medium
         df = DateFormat.getDateInstance(DateFormat.MEDIUM);
-        System.out.println("Formato Medium " + df.format(hoy));
+        System.out.println("Formato medium: " + df.format(hoy));
         
+        //Cambiamos el formateador al long
         df = DateFormat.getDateInstance(DateFormat.LONG);
-        System.out.println("Formato Long " + df.format(hoy));
+        System.out.println("Formato long: " + df.format(hoy));
         
+        //Cambiamos el formateador al full
         df = DateFormat.getDateInstance(DateFormat.FULL);
-        System.out.println("Formato full" + df.format(hoy));
+        System.out.println("Formato full: " + df.format(hoy));
         
-        df = DateFormat.getDateInstance(DateFormat.SHORT, DateFormat.MEDIUM, Locale.US);
-        System.out.println("Formato fecha/hora USA " df.format(hoy)
-    
-
-);
+        //Cambiamos el formateador para fecha y hora: (formato para fecha, formato para hora, lugar)
+        df = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.LONG, Locale.US);
+        System.out.println("Formato fehca/hora USA: " + df.format(hoy));
+        
     }
 
 }
--- Programacion/TEMA 4 Utilizacion de objetos/src/Ejercicios/Ejercicio_5.java
+++ Programacion/TEMA 4 Utilizacion de objetos/src/Ejercicios/Ejercicio_5.java
@@ -3,13 +3,27 @@
  ***************************************************************************** */
 package Ejercicios;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Scanner;
+
 /* programa que calcule el numero de dias trascurridos desde una fecha introducida por teclaso hasta hoy
 
 Ejemplo : 
 
 Introduce fecha 01/01/2022
 Han transcurrido hasta hoy 340 dias */
-
 public class Ejercicio_5 {
+
+    public static void main(String[] args) {
+
+        Scanner telado = new Scanner(System.in);
+        LocalDate hoy = LocalDate.now(); //Fecha de hoy
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/aaaa"); //Formateado con la fecha
+
+        System.out.print("Introduce fecha : ");
+        String fecha = telado.next();
+
+    }
 
 }
