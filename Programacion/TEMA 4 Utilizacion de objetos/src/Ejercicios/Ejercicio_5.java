b6f441b9032ff3ffa4ba21ef4ad6fb6ca0a5873f
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-11-17 13:24:04 +0100

    qa

--- Programacion/TEMA 4 Utilizacion de objetos/src/Ejercicios/Ejercicio_5.java
+++ Programacion/TEMA 4 Utilizacion de objetos/src/Ejercicios/Ejercicio_5.java
@@ -3,8 +3,8 @@
  ***************************************************************************** */
 package Ejercicios;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 /* programa que calcule el numero de dias trascurridos desde una fecha introducida por teclaso hasta hoy
@@ -17,13 +17,34 @@ public class Ejercicio_5 {
 
     public static void main(String[] args) {
 
-        Scanner telado = new Scanner(System.in);
-        LocalDate hoy = LocalDate.now(); //Fecha de hoy
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/aaaa"); //Formateado con la fecha
+        try {
+            Scanner telado = new Scanner(System.in);
 
-        System.out.print("Introduce fecha : ");
-        String fecha = telado.next();
+            System.out.print("Introduce fecha : ");
+            String Cadenafecha = telado.next();
 
+            DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT);
+            df.setLenient(false);
+
+            Date fecha = df.parse(Cadenafecha);
+            Date hoy = new Date();
+
+            if (fecha.after(hoy)) {
+                System.out.println("Debes introducir la fecha actual");
+
+            } else {
+
+                long milisegundos = hoy.getTime() - fecha.getTime();
+                long dias = milisegundos / 1000 / 60 / 60 / 24;
+                System.out.println("Dias transcurridos desde " df.format(fecha) + " hasta hoy " + dias
+            
+         );
+            
+        }
+        
+    } catch (Exception ex) {
+            System.out.println("Error");
+        }
     }
 
 }
