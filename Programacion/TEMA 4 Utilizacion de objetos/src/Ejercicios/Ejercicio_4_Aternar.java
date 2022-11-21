2ca77c3807840e75a47d53b627c75b54f7b807dd
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-11-21 23:31:28 +0100

    a

---
+++ Programacion/TEMA 4 Utilizacion de objetos/src/Ejercicios/Ejercicio_4_Aternar.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ ******************************************************************************/
+
+package Ejercicios;
+
+/**
+ *
+ * @author Eduardo Martin-Sonseca
+ */
+public class Ejercicio_4_Aternar {
+    
+    public static void main(String[] args) {
+        
+    }
+}
