01a45a71bd7ba2c96e4cca30086f4114e2f51e7e
eduardo <martinsonsecaeduardo@gmail.com>
2022-11-25 10:15:24 +0100

    h

--- Programacion/TEMA 4 Utilizacion de objetos/src/Ejemplos/FormateosNumericos.java
+++ Programacion/TEMA 4 Utilizacion de objetos/src/Ejemplos/FormateosNumericos.java
@@ -30,20 +30,31 @@ public class FormateosNumericos {
 //                nf=NumberFormat.getPercentInstance(Locale.US); //Para cambiar dre pais
 //                System.out.println(nf.format(cantidad));
         double cantidad1 = 123456.789;
-        double cantidad2 = -.456;
-        double cantidad3 = 123;
+//        double cantidad2 = -.456;
+//        double cantidad3 = 123;
+//
+//        DecimalFormat df = new DecimalFormat("0,000; 0,000.00"); // Plantilla (punto y coma para la separacion;
+//        System.out.println(df.format(cantidad1));
+//        System.out.println(df.format(cantidad2));
+//        System.out.println(df.format(cantidad3));
+//
+//        df.applyPattern("###.###; ##,###");
+//
+//        System.out.println(df.format(cantidad1));
+//        System.out.println(df.format(cantidad2));
+//        System.out.println(df.format(cantidad3));
 
-        DecimalFormat df = new DecimalFormat("0,000; 0,000.00"); // Plantilla (punto y coma para la separacion;
-        System.out.println(df.format(cantidad1));
-        System.out.println(df.format(cantidad2));
-        System.out.println(df.format(cantidad3));
-
-        df.applyPattern("###.###; ##,###");
-
-        System.out.println(df.format(cantidad1));
-        System.out.println(df.format(cantidad2));
-        System.out.println(df.format(cantidad3));
-
+        //FORMATO CON STRING
+        
+        System.out.println(String.format("%f", cantidad1)); //FORMATO POR DEFECTO
+        System.out.println(String.format("%30f", cantidad1)); //FORMATO POR tabulador
+        System.out.println(String.format("%-30f", cantidad1)); //FORMATO POR tabulador
+        
+        String  cadena = "Desarrollo de aplicaciones multiplataforma";
+        
+        System.out.println(String.format("%30s", cadena));
+        System.out.println(String.format("%-30s", cadena));
+        
     }
 
 }
