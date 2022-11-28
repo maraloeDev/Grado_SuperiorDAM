af343df9c4b01106d63d8a061140a11f018175d0
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-11-28 18:22:47 +0100

    fd

--- Programacion/1ra Evaluación/Practica/src/JuegosDeLosDados.java
+++ Programacion/1ra Evaluación/Practica/src/JuegosDeLosDados.java
@@ -23,43 +23,45 @@ public class JuegosDeLosDados {
 
         Random rndm = new Random();
         //Tirada de dados de la maquina y la suma de dados
+        //Generación aleatoria con el objeto Random
 
-        int suma_Dados_Maquina = rndm.nextInt(1, 7);
+        int suma_Dados_Maquina = rndm.nextInt(1, 7)
+                + rndm.nextInt(1, 7);
 
         //Tirada de dados del jugador y la suma de dados
-        int suma_Dados_Jugador = rndm.nextInt(1, 7);
+        int suma_Dados_Jugador = rndm.nextInt(1, 7)
+                + rndm.nextInt(1, 7);
 
         char respuesta;
 
-        int banca = 0;
+        int banca = 0; //Banca inicial
         int boteMaquina = 100; //bote inicial de la maquina
         int boteJugador = 100; //bote inicial del jugador
+
+        suma_Dados_Maquina = rndm.nextInt(1, 7)
+                + rndm.nextInt(1, 7);
+
+        suma_Dados_Jugador = rndm.nextInt(1, 7)
+                + rndm.nextInt(1, 7);
+
+        System.out.println("************************************\n"
+                + "Juego de los dados\n"
+                + "************************************\n"
+                + "Te atreves a jugar contra mí (S/N):");
+        respuesta = teclado.next().toUpperCase().trim().charAt(0);
+
+        //Tirada de salida //AQUI PUEDE HABER UN DO
+        System.out.println("Banca: " + banca + "\n"
+                + "Bote Maquina: " + boteMaquina + "\n"
+                + "Bote Jugador: " + boteJugador);
+        System.out.println("Empieza la partida el que saque más puntos");
+        System.out.println("Puntos Jugador:\n" + suma_Dados_Jugador
+                + "\n" + "Puntos Maquina: " + suma_Dados_Maquina);
+
         do {
-
-            suma_Dados_Maquina = rndm.nextInt(1, 7)
-                    + rndm.nextInt(1, 7);
-
-            suma_Dados_Jugador = rndm.nextInt(1, 7)
-                    + rndm.nextInt(1, 7);
-
-            System.out.println("************************************\n"
-                    + "Juego de los dados\n"
-                    + "************************************\n"
-                    + "Te atreves a jugar contra mí (S/N):");
-            respuesta = teclado.next().toUpperCase().trim().charAt(0);
-
-            System.out.println("Banca: " + banca + "\n"
-                    + "Bote Maquina: " + boteMaquina + "\n"
-                    + "Bote Jugador: " + boteJugador);
-
-            System.out.println("Empieza la partida el que saque más puntos");
-
-            //Tirada de salida
-            System.out.println("Puntos Jugador:\n" + suma_Dados_Jugador
-                    + "\n" + "Puntos Maquina: " + suma_Dados_Maquina);
-
             //Si gana la maquina, vuelve a tirar
             if (suma_Dados_Maquina > suma_Dados_Jugador) {
+                
                 suma_Dados_Maquina = rndm.nextInt(1, 7)
                         + rndm.nextInt(1, 7);
 
@@ -69,7 +71,7 @@ public class JuegosDeLosDados {
                 System.out.println("Puntos Jugador:\n" + suma_Dados_Jugador
                         + "\n" + "Puntos Maquina: " + suma_Dados_Maquina);
 
-                System.out.println("Empieza la maquina");
+                System.out.println("EMPIEZA LA MAQUINA");
                 System.out.println("Lo siento. He ganado");
 
                 System.out.println("Banca: " + (banca + 15) + " euros"
@@ -79,9 +81,9 @@ public class JuegosDeLosDados {
                 System.out.println("Continuamos jugando (S/N):");
                 respuesta = teclado.next().toUpperCase().trim().charAt(0);
 
-            } else if (suma_Dados_Maquina > suma_Dados_Jugador) {
+            } else if (suma_Dados_Jugador > suma_Dados_Maquina) {
 
-                //Si gana el jugador, vuelve a tirar
+                //Si gana la jugador, vuelve a tirar
                 suma_Dados_Maquina = rndm.nextInt(1, 7)
                         + rndm.nextInt(1, 7);
 
@@ -91,7 +93,7 @@ public class JuegosDeLosDados {
                 System.out.println("Puntos Jugador:\n" + suma_Dados_Jugador
                         + "\n" + "Puntos Maquina: " + suma_Dados_Maquina);
 
-                System.out.println("Empieza la Maquina");
+                System.out.println("EMPIEZA EL JUGADOR");
                 System.out.println("Enhorabuena. Has ganado");
 
                 System.out.println("Banca: " + (banca + 15) + " euros"
