d95a1af1be07095200e4d4532f1499a2f7ec4ad0
eduardo <martinsonsecaeduardo@gmail.com>
2022-12-07 13:47:07 +0100

    rtg

--- Programacion/2da Evaluación/TEMA 5/src/Ejemplos/Alumno.java
+++ Programacion/2da Evaluación/TEMA 5/src/Ejemplos/Alumno.java
@@ -1,6 +1,8 @@
-/** *****************************************************************************
+/**
+ * ****************************************************************************
  * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
- ***************************************************************************** */
+ * ****************************************************************************
+ */
 package Ejemplos;
 
 import java.time.LocalDate;
@@ -9,11 +11,71 @@ import java.time.LocalDate;
  *
  * @author maraloed
  */
+
+//ATTRIBUTES
 public class Alumno {
     private String num_matricula;
     private String nombre;
     private String apellidos;
     private LocalDate fecha_nacimiento;
     private double nota;
-    
+
+    //CONSTRUCTORS
+
+    public Alumno() {
+    }
+
+    //GETTER AND SETTER
+    public String getNum_matricula() {
+        return num_matricula;
+    }
+
+    public void setNum_matricula(String num_matricula) {
+        this.num_matricula = num_matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public LocalDate getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(LocalDate fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "num_matricula='" + num_matricula + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", fecha_nacimiento=" + fecha_nacimiento +
+                ", nota=" + nota +
+                '}';
+    }
 }
