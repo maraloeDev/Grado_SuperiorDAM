cd63b4cb5ee975741023cc99a436f5eb026be150
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-12-18 00:44:51 +0100

    u

--- Programacion/1ra Evaluación/Examen/src/Problema1/Problema1.java
+++ Programacion/1ra Evaluación/Examen/src/Problema1/Problema1.java
@@ -1,44 +1,43 @@
+/*
+ * Copyright (c) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ */
+
 package Problema1;
 
-import java.util.Random;
 import java.util.Scanner;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author maraloed
- */
 public class Problema1 {
 
     public static void main(String[] args) {
-        //Creación del objeto Scanner para la introduccion de datos por parte del usuario
-        Scanner teclado = new Scanner(System.in);
-        double adivina;
-        int numero;
-        //Inicio de la pregunta
-        System.out.print("El numero secreto está entre 1 y 100\n"
-                + "Cual es ");
-        numero = teclado.nextInt();
-        do {
-            //Generación de aleatorios emtre 1 y 100 mediante el objeto Random
-            adivina = (Math.random() * 99) +1;
-            //Cuando falla la pregunta, aparecera la siguiente frase
-            System.out.println("Lo siento...");
-            System.out.print("El numero secreto está entre 1 y 100\n"
-                    + "Cual es ");
-            numero = teclado.nextInt();
-            if (numero < 0 || numero > 100) {
-                System.out.println("FUERA DE RANGO");
-                System.out.print("El numero secreto está entre 1 y 100\n"
-                        + "Cual es ");
-                numero = teclado.nextInt();
+    Scanner teclado = new Scanner(System.in);
+        int adivina = (int) (Math.random() * 100) + 1; //Numero aleatorio entre 1 y 100
+    int minimo=1; //Numero minimo
+    int maximo=100; //Numero maximo
+    boolean acierto =false;
+
+    while (acierto==false){
+        System.out.println("El nº secreto está entre " + minimo + " y " + maximo + "\n" +
+                "¿Cuál es?");
+        int numero = teclado.nextInt();
+
+        if (numero==adivina){
+            System.out.println("Acertaste!!");
+            acierto=true;
+
+        } else {
+            if (adivina > (minimo + maximo) /2){
+                minimo= (int) (minimo + maximo) / 2;
+                System.out.println("El nº secreto está entre " + minimo + " y " + maximo + "\n" +
+                        "¿Cuál es?");
+
+            } else {
+                maximo= (int) (minimo + maximo) / 2;
+                System.out.println("El nº secreto está entre " + minimo + " y " + maximo + "\n" +
+                        "¿Cuál es?");
             }
 
-        } while (numero != (int) adivina);
-        System.out.println("Acertaste!!");
+        }
     }
 
+    }
 }
