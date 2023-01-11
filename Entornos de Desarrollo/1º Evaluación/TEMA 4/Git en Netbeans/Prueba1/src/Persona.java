96b093983317d92c3e479467e86c8a5c657421d4
eduardo <martinsonsecaeduardo@gmail.com>
2023-01-11 10:07:03 +0100

    kj

---
+++ Entornos de Desarrollo/1º Evaluación/TEMA 4/Git en Netbeans/Prueba1/src/Persona.java
@@ -0,0 +1,43 @@
+
+/** *****************************************************************************
+ * Copyright (C) EDUARDO MARTÍN-SONSECA (maraloeDev)
+ ***************************************************************************** */
+/**
+ *
+ * @author maraloed
+ */
+public class Persona {
+    
+    private String a;
+    private String b;
+    private String c;
+
+    public Persona() {
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+    
+    
+}
